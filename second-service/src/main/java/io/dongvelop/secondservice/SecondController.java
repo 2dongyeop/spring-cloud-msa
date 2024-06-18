2ede5701c2d8ee73c9ecd922f1801fb529a39801
leedongyeop <ldy_1204@naver.com>
2024-06-18 20:58:40 +0900

    chore : Spring Cloud Gateway 프로젝트 생성

--- second-service/src/main/java/io/dongvelop/secondservice/SecondController.java
+++ second-service/src/main/java/io/dongvelop/secondservice/SecondController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @description
  */
 @RestController
-@RequestMapping("/")
+@RequestMapping("/second-service")
 public class SecondController {
 
     @GetMapping("/welcome")
