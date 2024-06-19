46ca5ee14d738833365ed82bcb11b3f0fc4e4013
leedongyeop <ldy_1204@naver.com>
2024-06-19 18:56:07 +0900

    feat : Spring Cloud Gateway - Load Balancer

--- first-service/src/main/java/io/dongvelop/firstservice/FirstController.java
+++ first-service/src/main/java/io/dongvelop/firstservice/FirstController.java
@@ -1,11 +1,14 @@
 package io.dongvelop.firstservice;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 
 /**
@@ -16,7 +19,10 @@ import java.time.LocalDateTime;
 @Slf4j
 @RestController
 @RequestMapping("/first-service")
+@RequiredArgsConstructor
 public class FirstController {
+
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -30,7 +36,9 @@ public class FirstController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from first service";
+    public String check(HttpServletRequest request) {
+        log.info("Server port : {}", request.getServerPort());
+        return String.format("Hi, there. This is a message from first service on PORT %s",
+                env.getProperty("local.server.port"));
     }
 }
